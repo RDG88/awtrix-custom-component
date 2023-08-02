a9f7f8e7595173e3bcd92cd87bf9cc781e2ba820
Rob de Graaf <r.degraaf@cam.nl>
2023-08-02 21:33:00 +0200

    try

--- light.py
+++ light.py
@@ -16,23 +16,28 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     name = config['host']
 
     lights = [
-        CustomLight('main_light', name, topic, 'state', 'power'),
+        CustomLight(
+            name="Matrix",
+            state_topic=f"{topic}/stats",
+            command_topic=f"{topic}/power",
+            color_topic=f"{topic}/settings",
+            brightness_topic=f"{topic}/settings",
+        ),
     ]
 
     async_add_entities(lights)
 
 class CustomLight(LightEntity):
-    def __init__(self, id, device_name, topic, attr, command_topic):
+    def __init__(self, name, state_topic, command_topic, color_topic, brightness_topic):
         """Initialize the light."""
         self._state = None
         self._brightness = 255  # Set default brightness to maximum
         self._rgb_color = (255, 255, 255)  # Set default color to white
-        self._attr = attr
-        self._name = f"{device_name} {id}"
-        self._state_topic = f"{topic}/stats"
-        self._command_topic = f"{topic}/{command_topic}"
-        self._color_topic = f"{topic}/settings"  # Add this line
-        self._brightness_topic = f"{topic}/settings"
+        self._name = name
+        self._state_topic = state_topic
+        self._command_topic = command_topic
+        self._color_topic = color_topic
+        self._brightness_topic = brightness_topic
 
     async def async_added_to_hass(self):
         """Call when entity is added to hass."""
@@ -82,8 +87,8 @@ class CustomLight(LightEntity):
     def _message_received(self, msg):
         """Handle new MQTT messages."""
         data = json.loads(msg.payload)
-        if self._attr in data:
-            self._state = data[self._attr]
+        if 'state' in data:
+            self._state = data['state']
             self.async_write_ha_state()
             _LOGGER.warning("Received an update for %s from MQTT topic %s. New state is %s", self._name, self._topic, self._state)
 
@@ -95,10 +100,7 @@ class CustomLight(LightEntity):
             await mqtt.async_publish(self.hass, self._brightness_topic, json.dumps({"BRI": self._brightness}), qos=1)
         if ATTR_RGB_COLOR in kwargs:
             self._rgb_color = kwargs[ATTR_RGB_COLOR]
-            r, g, b = self._rgb_color
-            # Convert RGB color to hex string
-            hex_color = '#{:02x}{:02x}{:02x}'.format(r, g, b)
-            await mqtt.async_publish(self.hass, self._color_topic, json.dumps({"TCOL": hex_color}), qos=1)
+            await mqtt.async_publish(self.hass, self._color_topic, json.dumps({"TCOL": self._rgb_color}), qos=1)
         await mqtt.async_publish(self.hass, self._command_topic, json.dumps({"power": True}), qos=1)
         self._state = True
         self.async_write_ha_state()
